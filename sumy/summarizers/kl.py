81ff455590813f8d9fc4399fe01ab638561bb3a3
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-06-05 15:29:52 +0000

    Fix review issue #8: Lift 6 shared word-utility methods from KL/SumBasic to AbstractSummarizer
    KLSummarizer and SumBasicSummarizer contained byte-for-byte identical
    implementations of _get_all_words_in_doc, _get_content_words_in_sentence,
    _normalize_words, _filter_out_stop_words, _compute_word_freq, and
    _get_all_content_words_in_doc. Moved all six to AbstractSummarizer where
    they are inherited by both subclasses.

--- sumy/summarizers/kl.py
+++ sumy/summarizers/kl.py
@@ -23,32 +23,6 @@ class KLSummarizer(AbstractSummarizer):
         ratings = self._compute_ratings(sentences)
         return ratings
 
-    def _get_all_words_in_doc(self, sentences):
-        return [w for s in sentences for w in s.words]
-
-    def _get_content_words_in_sentence(self, sentence):
-        normalized_words = self._normalize_words(sentence.words)   
-        normalized_content_words = self._filter_out_stop_words(normalized_words)
-        return normalized_content_words
-
-    def _normalize_words(self, words):
-        return [self.normalize_word(w) for w in words]
-
-    def _filter_out_stop_words(self, words):
-        return [w for w in words if w not in self.stop_words]
-
-    def _compute_word_freq(self, list_of_words):
-        word_freq = {}
-        for w in list_of_words:
-            word_freq[w] = word_freq.get(w, 0) + 1
-        return word_freq
-
-    def _get_all_content_words_in_doc(self, sentences):
-        all_words = self._get_all_words_in_doc(sentences)
-        content_words = self._filter_out_stop_words(all_words)
-        normalized_content_words = self._normalize_words(content_words)
-        return normalized_content_words
-        
     def _compute_tf(self, sentences):
         '''
         Computes the normalized term frequency as explained in http://www.tfidf.com/
