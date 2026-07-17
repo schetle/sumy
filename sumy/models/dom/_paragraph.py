9ff1421cad3d34097415b612f2c94e879966aaf5
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-07-17 18:35:11 +0000

    feat: add Python 3.9+ type annotations to Paragraph class
    Annotate __init__, sentences, headings, words, __str__, and __repr__
    with full parameter and return types. Annotate _sentences attribute.

--- sumy/models/dom/_paragraph.py
+++ sumy/models/dom/_paragraph.py
@@ -4,31 +4,31 @@ from ._sentence import Sentence
 
 
 class Paragraph(object):
-    def __init__(self, sentences):
+    def __init__(self, sentences: list[Sentence] | tuple[Sentence, ...]) -> None:
         sentences = tuple(sentences)
         for sentence in sentences:
             if not isinstance(sentence, Sentence):
                 raise TypeError("Only instances of class 'Sentence' are allowed.")
 
-        self._sentences = sentences
+        self._sentences: tuple[Sentence, ...] = sentences
 
     @cached_property
-    def sentences(self):
+    def sentences(self) -> tuple[Sentence, ...]:
         return tuple(s for s in self._sentences if not s.is_heading)
 
     @cached_property
-    def headings(self):
+    def headings(self) -> tuple[Sentence, ...]:
         return tuple(s for s in self._sentences if s.is_heading)
 
     @cached_property
-    def words(self):
+    def words(self) -> tuple[str, ...]:
         return tuple(chain(*(s.words for s in self._sentences)))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "<Paragraph with %d headings & %d sentences>" % (
             len(self.headings),
             len(self.sentences),
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
